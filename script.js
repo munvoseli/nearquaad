e0ae98b159d75a7f7c852c883eccf067ec8f4bdd
munvoseli <muslinmelody@gmail.com>
2022-10-27 17:44:44 -0400

    YEY websock handshake

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 'use strict';
 console.log("h");
+
+let wspath = "ws://" + window.location.hostname + ":" + window.location.port + "/webs";
+let ws = new WebSocket(wspath);
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,79 @@
 use hyper::{Body, Request, Response};
-use hyper::service::service_fn;
+use std::convert::Infallible;
+use hyper::service::{make_service_fn, service_fn};
 use std::io::Read;
 
-async fn handle(req: Request<Body>) -> Result<Response<Body>, std::convert::Infallible> {
+async fn handle(mut req: Request<Body>) -> Result<Response<Body>, std::convert::Infallible> {
 	println!("Handling function; path is {}", req.uri());
-	let file = std::fs::File::open(&req.uri().path()[1..]);
+	let uri = &req.uri().path()[1..];
+	let file = std::fs::File::open(uri);
 	if let Ok(mut file) = file {
 		let mut buf = Vec::new();
 		file.read_to_end(&mut buf).unwrap();
-		Ok(Response::new(Body::from(buf)))
+		if uri[uri.len()-2..] == *"js" {
+			Ok(Response::builder()
+			 .status(200)
+			 .header("Content-Type", "application/javascript")
+			 .body(Body::from(buf))
+			 .unwrap())
+		} else {
+			Ok(Response::new(Body::from(buf)))
+		}
+	} else if uri == "webs" {
+		let headers = req.headers();
+		let reqkey = &headers["Sec-WebSocket-Key"];
+		let retkey = tungstenite::handshake::derive_accept_key(reqkey.as_bytes());
+		tokio::spawn(async move {
+			match hyper::upgrade::on(&mut req).await {
+				Ok(upgraded) => {
+					let strem = tokio_tungstenite::WebSocketStream::from_raw_socket(upgraded, tungstenite::protocol::Role::Server, None);
+					println!("upgrade succeeded");
+				},
+				Err(e) => eprintln!("upgrade error: {}", e)
+			}
+		});
+		Ok(Response::builder()
+		 .status(101)
+		 .header("Upgrade", "websocket")
+		 .header("Connection", "Upgrade")
+		 .header("Sec-WebSocket-Accept", retkey)
+		 .body(Body::empty())
+		 .unwrap())
 	} else {
-		Ok(Response::new(Body::from("Hello, World")))
+		Ok(Response::builder()
+		 .status(404)
+		 .body(Body::from("404 Eroor"))
+		 .unwrap())
 	}
 }
 
 #[tokio::main]
 async fn main() {
 	let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 8080));
-	let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-	loop {
-		let (stream, _) = listener.accept().await.unwrap();
-		tokio::spawn(async move {
-			let http = hyper::server::conn::Http::new();
-			if let Err(err) = http.serve_connection(stream, service_fn(handle)).with_upgrades().await {
-				eprintln!("Error serving stream, {}", err);
-			}
-			println!("Finished with stream");
-		});
+	let service = service_fn(handle);
+	let make_service = make_service_fn(|_conn| async move {
+		Ok::<_, Infallible>(service)
+	});
+	let server = hyper::Server::bind(&addr).serve(make_service);
+	if let Err(e) = server.await {
+		eprintln!("server error: {}", e);
 	}
 }
 
+//#[tokio::main]
+//async fn main() {
+//	let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 8080));
+//	let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+//	loop {
+//		let (stream, _) = listener.accept().await.unwrap();
+//		tokio::spawn(async move {
+//			let http = hyper::server::conn::Http::new();
+//			let service = service_fn(handle);
+//			let conn = http.serve_connection(stream, service).with_upgrades();
+//			if let Err(err) = conn.await {
+//				eprintln!("Error serving stream, {}", err);
+//			}
+//			println!("Finished with stream");
+//		});
+//	}
+//}
