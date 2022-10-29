c955cd0aeac2b952c1c73831843873d0318673a6
munvoseli <muslinmelody@gmail.com>
2022-10-29 08:02:15 -0400

    eyeyey trianglesgit add index.html

--- script.js
+++ script.js
@@ -1,8 +1,163 @@
 'use strict';
 console.log("h");
 
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 let wspath = "ws://" + window.location.hostname + ":" + window.location.port + "/webs";
 let ws = new WebSocket(wspath);
+ctx.fillStyle = "#000";
+
+let points = {};
+let pendingPoints = [];
+let lines = [];
+let tris = [];
+
+function xyToPoint(x, y) {
+	let keys = [];
+	for (let k in points) {
+		keys.push(k);
+	}
+	let mind = (x - points[keys[0]].x) ** 2 + (y - points[keys[0]].y) ** 2;
+	let mini = keys[0];
+	for (let i = 1; i < keys.length; ++i) {
+		let d = (x - points[keys[i]].x) ** 2 + (y - points[keys[i]].y) ** 2;
+		if (d < mind) {
+			mind = d;
+			mini = keys[i];
+		}
+	}
+	return mini;
+}
+
+let lastClicked = [];
+let curx, cury;
+
+canvas.addEventListener("mousemove", function(e) {
+	let r = this.getBoundingClientRect();
+	curx = (e.clientX - r.x) / r.width;
+	cury = (e.clientY - r.y) / r.height;
+}, false);
+addEventListener("keydown", function(e) {
+	if (e.key == "t") {
+		if (lastClicked.length < 3) return;
+		ws.send(["makeTri", lastClicked[0], lastClicked[1], lastClicked[2]].join(" "));
+	}
+	console.log(e);
+}, false);
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.beginPath();
+	ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+	for (let tri of tris) {
+		ctx.moveTo(points[tri[0]].x * canvas.width, points[tri[0]].y * canvas.height);
+		ctx.lineTo(points[tri[1]].x * canvas.width, points[tri[1]].y * canvas.height);
+		ctx.lineTo(points[tri[2]].x * canvas.width, points[tri[2]].y * canvas.height);
+	}
+	ctx.fill();
+	ctx.closePath();
+	ctx.beginPath();
+	ctx.fillStyle = "#000";
+	for (let pi in points) {
+		let point = points[pi];
+		ctx.fillRect(point.x * canvas.width - 5, point.y * canvas.height - 5, 10, 10);
+	}
+	for (let line of lines) {
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.lineWidth = line[2] == 1 ? 5 : 2;
+		ctx.moveTo(points[line[0]].x * canvas.width, points[line[0]].y * canvas.height);
+		ctx.lineTo(points[line[1]].x * canvas.width, points[line[1]].y * canvas.height);
+		ctx.stroke();
+		ctx.closePath();
+	}
+}
+
+canvas.addEventListener("mousedown", function(e) {
+	let r = this.getBoundingClientRect();
+	let x = (e.clientX - r.x) / r.width;
+	let y = (e.clientY - r.y) / r.height;
+	if (e.button == 0) {
+		pendingPoints.push({x: x, y: y});
+		draw();
+		if (ws.OPEN) {
+			ws.send(["placePoint", x, y].join(" "));
+		}
+	} else {
+		console.log(lastClicked);
+		lastClicked.unshift(xyToPoint(x, y));
+		if (lastClicked.length > 3) lastClicked.pop();
+	}
+	console.log(e);
+	e.cancelBubble = true;
+	e.preventDefault();
+	return false;
+}, false);
+canvas.addEventListener("contextmenu", function(e) {
+	e.preventDefault();
+	return false;
+}, false);
+
+
 ws.onmessage = function(e) {
 	console.log(e.data);
+	let terms = e.data.split(" ");
+	let i = 0;
+	for (;;) {
+		if (i >= terms.length) break;
+		let cmd = terms[i++];
+		switch (cmd) {
+		case "setPoints": {
+			let len = parseInt(terms[i++]);
+			points = [];
+			for (let j = 0; j < len; ++j) {
+				let pi = parseInt(terms[i++]);
+				let x = parseFloat(terms[i++]);
+				let y = parseFloat(terms[i++]);
+				points[pi] = {x: x, y: y};
+			}
+		} break;
+		case "setLines": {
+			let len = parseInt(terms[i++]);
+			lines = [];
+			for (let j = 0; j < len; ++j) {
+				let pia = parseInt(terms[i++]);
+				let pib = parseInt(terms[i++]);
+				let lc = parseInt(terms[i++]);
+				lines.push([pia, pib, lc]);
+			}
+		} break;
+		case "setTris": {
+			let len = parseInt(terms[i++]);
+			tris = [];
+			for (let j = 0; j < len; ++j) {
+				let pia = parseInt(terms[i++]);
+				let pib = parseInt(terms[i++]);
+				let pic = parseInt(terms[i++]);
+				tris.push([pia, pib, pic]);
+			}
+		} break;
+		case "placePoint": {
+			let pi = parseInt(terms[i++]);
+			let x = parseFloat(terms[i++]);
+			let y = parseFloat(terms[i++]);
+			points[pi] = {x: x, y: y};
+			draw();
+		} break;
+		case "connectPoints": {
+			let pia = parseInt(terms[i++]);
+			let pib = parseInt(terms[i++]);
+			lines.push([pia, pib]);
+			draw();
+		} break;
+		case "makeTri": {
+			let pia = parseInt(terms[i++]);
+			let pib = parseInt(terms[i++]);
+			let pic = parseInt(terms[i++]);
+			tris.push([pia, pib, pic]);
+			draw();
+		} break;
+		}
+		draw();
+	}
 };
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use hyper::{Body, Request, Response};
 use std::convert::Infallible;
 use hyper::service::{make_service_fn, service_fn};
+use futures_util::{StreamExt, SinkExt};
 use std::io::Read;
 use futures::Sink;
 use tokio_tungstenite::WebSocketStream;
 use std::sync::Arc;
 use std::sync::Mutex;
-use futures_util::{StreamExt, SinkExt};
-//use futures_util::sink::Sink;
 use hyper::upgrade::Upgraded;
+use std::collections::{HashMap, HashSet};
 
 struct Apioform {
 	sink: futures_util::stream::SplitSink<WebSocketStream<Upgraded>, tungstenite::Message>,
@@ -16,7 +16,7 @@ struct Apioform {
 //	strs: Arc<Mutex<Vec<String>>>
 }
 
-async fn handle(mut req: Request<Body>, apioforms: Arc<Mutex<Vec<Apioform>>>, tx: tokio::sync::mpsc::Sender<(usize, String)>, api: usize) -> Result<Response<Body>, std::convert::Infallible> {
+async fn handle(mut req: Request<Body>, apioforms: Arc<Mutex<Vec<Apioform>>>, tx: tokio::sync::mpsc::Sender<(usize, Option<String>)>, api: usize) -> Result<Response<Body>, std::convert::Infallible> {
 	println!("Handling function; path is {}", req.uri());
 	let uri = &req.uri().path()[1..];
 	let file = std::fs::File::open(uri);
@@ -49,10 +49,10 @@ async fn handle(mut req: Request<Body>, apioforms: Arc<Mutex<Vec<Apioform>>>, tx
 					}
 					while let Some(Ok(tungstenite::Message::Text(message))) = strem.next().await {
 						println!("received message {}", message);
-						tx.send((api, message)).await.unwrap();
-						println!("tx finished");
+						tx.send((api, Some(message))).await.unwrap();
 					}
-					println!("upgrade succeeded");
+					tx.send((api, None)).await.unwrap();
+					println!("ws connection closed");
 				},
 				Err(e) => eprintln!("upgrade error: {}", e)
 			}
@@ -72,6 +72,86 @@ async fn handle(mut req: Request<Body>, apioforms: Arc<Mutex<Vec<Apioform>>>, tx
 	}
 }
 
+struct WorldData {
+	points: HashMap<usize, (String, String)>,
+	pi: usize, // key for hashmap, point id, incremented after each placement
+	// the amount of connections between two points
+	// the idea is that at most two polygons can border a line
+	// the u8 should either be 1 or 2
+	lines: HashMap<[usize; 2], u8>,
+	tris: HashSet<[usize; 3]>,
+	quads: HashSet<[usize; 4]>,
+}
+
+impl WorldData {
+	pub fn new() -> WorldData {
+		WorldData {
+			points: HashMap::new(),
+			lines: HashMap::new(),
+			tris: HashSet::new(),
+			quads: HashSet::new(),
+			pi: 0
+		}
+	}
+}
+
+fn place_point(wd: &mut WorldData, tokens: &mut Vec<String>, x: &str, y: &str) {
+	wd.points.insert(wd.pi, (x.to_string(), y.to_string()));
+	tokens.push("placePoint".to_string());
+	tokens.push(wd.pi.to_string());
+	wd.pi += 1;
+	tokens.push(x.to_string());
+	tokens.push(y.to_string());
+}
+
+fn points_to_line(wd: &mut WorldData, tokens: &mut Vec<String>, pia: usize, pib: usize) {
+	let mut v = vec![pia, pib]; v.sort();
+	if let Some(l) = wd.lines.get(&[v[0], v[1]]) {
+		wd.lines.insert([v[0], v[1]], l + 1);
+	} else {
+		wd.lines.insert([v[0], v[1]], 1);
+	}
+	tokens.push("connectPoints".to_string());
+	tokens.push(v[0].to_string());
+	tokens.push(v[1].to_string());
+}
+
+fn points_to_tri(wd: &mut WorldData, tokens: &mut Vec<String>, pia: usize, pib: usize, pic: usize) {
+	let mut v = vec![pia, pib, pic]; v.sort();
+	points_to_line(wd, tokens, pia, pib);
+	points_to_line(wd, tokens, pia, pic);
+	points_to_line(wd, tokens, pib, pic);
+	wd.tris.insert([v[0], v[1], v[2]]);
+	tokens.push("makeTri".to_string());
+	tokens.push(v[0].to_string());
+	tokens.push(v[1].to_string());
+	tokens.push(v[2].to_string());
+}
+
+fn dump_wd(wd: &mut WorldData, tokens: &mut Vec<String>) {
+	tokens.push("setPoints".to_string());
+	tokens.push(wd.points.len().to_string());
+	for (pi, xy) in &wd.points {
+		tokens.push(pi.to_string());
+		tokens.push(xy.0.to_string());
+		tokens.push(xy.1.to_string());
+	}
+	tokens.push("setLines".to_string());
+	tokens.push(wd.lines.len().to_string());
+	for (pii, lc) in &wd.lines {
+		tokens.push(pii[0].to_string());
+		tokens.push(pii[1].to_string());
+		tokens.push(lc.to_string());
+	}
+	tokens.push("setTris".to_string());
+	tokens.push(wd.tris.len().to_string());
+	for tri in &wd.tris {
+		tokens.push(tri[0].to_string());
+		tokens.push(tri[1].to_string());
+		tokens.push(tri[2].to_string());
+	}
+}
+
 #[tokio::main]
 async fn main() {
 	let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 8080));
@@ -88,33 +168,63 @@ async fn main() {
 			let apj = api.clone();
 			let mut apj = apj.lock().unwrap();
 			*apj += 1;
-			println!("{}", *apj);
 			handle(req, apioforms.clone(), tx.clone(), *apj)
 		});
 		async move { Ok::<_, Infallible>(service) }
 	});
 	tokio::spawn(async move {
-		let mut points: Vec<(usize, String, String)> = Vec::new();
-		let mut pi: usize = 0;
+		let mut wd = WorldData::new();
 		while let Some((api, message)) = rx.recv().await {
+			if message.is_none() {
+				// stream closed
+				let mut apios = apios.lock().unwrap();
+				let mut rem = false;
+				for i in 0..apios.len() {
+					if apios[i].id == api {
+						println!("remove conn no. {}  ({}-th)", api, i);
+						apios.remove(i);
+						rem = true;
+						break;
+					}
+				}
+				if !rem {
+					eprintln!("did not remove conn no. {}", api);
+				}
+				continue;
+			}
+			let message = message.unwrap();
 			let mut tokens: Vec<&str> = message.split(' ').collect();
-			// placePoint/2
-			// placePointOnLine/4
-			// connectPoint/2
+			// client to server:
+			// placePoint () x, y
+			// connectPoints () pi, pi
+			// disconnPoints () pi, pi
+			// lineToTri () pi, pi, x, y
+			// triToQuad () pi, pi, pif, x, y
+			// makeTriXY () pi, pi, x, y
+			// makeTri () pi, pi, pi
 			loop {
 				if tokens.len() == 0 { break; }
 				let cmd = tokens.remove(0);
-				let (selfmsg, awaymsg) = if cmd == "placePoint" {
+				let mut outok: Vec<String> = Vec::new();
+				match cmd {
+				"placePoint" => {
 					let x = tokens.remove(0);
 					let y = tokens.remove(0);
-					points.push((pi, x.to_string(), y.to_string())); pi += 1;
-					(format!("pointIndex {} {} {}", pi - 1, x, y), format!("placePoint {} {} {}", pi - 1, x, y))
-				} else {
-					(String::new(), String::new())
+					place_point(&mut wd, &mut outok, x, y);
+				},
+				"makeTri" => {
+					let pia = tokens.remove(0).parse::<usize>().unwrap();
+					let pib = tokens.remove(0).parse::<usize>().unwrap();
+					let pic = tokens.remove(0).parse::<usize>().unwrap();
+					points_to_tri(&mut wd, &mut outok, pia, pib, pic);
+				},
+				_ => {}
 				};
+				outok = Vec::new();
+				dump_wd(&mut wd, &mut outok);
+				let msg = outok.join(" ");
 				let mut i: usize = 0;
 				loop {
-					println!("apio started");
 					let mut apio = {
 						let mut apios = apios.lock().unwrap();
 						if i >= apios.len() {
@@ -122,16 +232,16 @@ async fn main() {
 						}
 						apios.remove(i)
 					};
-					if api == apio.id {
-						apio.sink.send(tungstenite::Message::Text(selfmsg.to_string())).await.unwrap();
-					} else {
-						apio.sink.send(tungstenite::Message::Text(awaymsg.to_string())).await.unwrap();
-					}
+					apio.sink.send(tungstenite::Message::Text(msg.to_string())).await.unwrap();
+					//if api == apio.id {
+					//	apio.sink.send(tungstenite::Message::Text(selfmsg.to_string())).await.unwrap();
+					//} else {
+					//	apio.sink.send(tungstenite::Message::Text(awaymsg.to_string())).await.unwrap();
+					//}
 					{
 						let mut apios = apios.lock().unwrap();
 						apios.insert(i, apio);
 					}
-					println!("apio finished");
 					i += 1;
 				}
 			}
